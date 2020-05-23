bbbcf0bb1e71281bb96c9f39f4094a8c04752995
codydeny <codydeny@gmail.com>
2020-05-24 00:54:06 +0530

    Updated static root

--- PlaceMe/settings.py
+++ PlaceMe/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '_staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
